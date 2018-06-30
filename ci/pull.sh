f1ad34557a5e48718e58ec91e15fa3e22c00aef3
mymindstorm <mymindstorm1@gmail.com>
2018-06-30 16:30:06 -0500

    Fix issues

--- ci/pull.sh
+++ ci/pull.sh
@@ -8,6 +8,7 @@ if ! curl -s 'https://api.travis-ci.org/numixproject/numix-core.svg?branch=maste
     exit 1
 fi
 
+export 
 mkdir -p $TRAVIS_BUILD_DIR/build
-rm -rf $TRAVIS_BUILD_DIR/build/* $TRAVIS_BUILD_DIR/Numix-${VARIANT^}/48
+rm -rf $TRAVIS_BUILD_DIR/build/* $TRAVIS_BUILD_DIR/Numix-$VARIANT_UPPER/48
 git clone https://github.com/numixproject/numix-core.git $TRAVIS_BUILD_DIR/build
